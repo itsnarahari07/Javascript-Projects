95a71094ecd908ad8e5438977df65652305ac5ff
Narahari_P <119927531+itsnarahari07@users.noreply.github.com>
2026-09-19 18:26:07 +0530

    Shopping Cart Project

--- Shopping Cart/script.js
+++ Shopping Cart/script.js
@@ -1,4 +1,7 @@
 let buttons = document.querySelectorAll(".productButton")
+let cart = [];
+
+let total = 0;
 
 buttons.forEach((b) => {
     b.addEventListener("click", function() {
@@ -6,8 +9,38 @@ buttons.forEach((b) => {
         let card = this.parentElement.parentElement;
         let name = card.querySelector(".productname");
         let fname = name.textContent;
+        let price = card.querySelector(".productPrice");
+        let pprice = price.textContent;
 
-        alert(`${fname} was added to cart`);
+        let numprice = parseFloat(pprice.replace("$",""));
+        cart.push({
+            name : fname,
+            cost : numprice
+        })
+        console.log(cart);
         
+        total = total + numprice;
+        console.log(total);
+        
+        let carttotal = document.querySelector(".cartTotal");
+
+        let createLi = document.createElement("li");
+        let ol = document.querySelector(".listOfFruits");
+        ol.appendChild(createLi);
+
+        createLi.textContent = `Fruit Name : ${fname} Value : $${numprice}`;
+        carttotal.textContent = `Total Cart Value : $${total}`;
+
+        let removebutton = document.createElement("button");
+        removebutton.textContent = "remove item";
+
+        createLi.appendChild(removebutton);
+
+        removebutton.addEventListener("click", function() {
+            this.parentElement.remove();
+            total = total - numprice;
+            carttotal.textContent = `Total Cart Value : $${total}`;
+        })
+
     });
 });
